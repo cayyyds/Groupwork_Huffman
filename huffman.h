e79035d977d8c99330f691562274d0d3e14bd5ca
cayyyds <122010571+cayyyds@users.noreply.github.com>
2025-11-19 20:21:13 +0800

    minheap

--- huffman.h
+++ huffman.h
@@ -22,10 +22,48 @@ class HuffTree {
 };
 
 // -------- 最小堆，用于构建 Huffman 树 ----------
+//czy编写
 template <typename T>
-class MinHeap {
 
+class MinHeap {
+public:
+    HuffTree<T> data[NUMBER];
+    int size;
+
+    MinHeap() { size = 0; }
+
+    void insert(const HuffTree<T>& t) {
+        data[++size] = t;
+        int i = size;
+        while (i > 1 && data[i].weight() < data[i/2].weight()) {
+            swap(data[i], data[i/2]);
+            i /= 2;
+        }
+    }
+
+    HuffTree<T> removeFirst() {
+        HuffTree<T> top = data[1];
+        data[1] = data[size--];
+        int i = 1;
+
+        while (true) {
+            int smallest = i;
+            int l = 2*i, r = 2*i + 1;
+
+            if (l <= size && data[l].weight() < data[smallest].weight())
+                smallest = l;
+            if (r <= size && data[r].weight() < data[smallest].weight())
+                smallest = r;
+
+            if (smallest == i) break;
+            swap(data[i], data[smallest]);
+            i = smallest;
+        }
+        return top;
+    }
 };
+
+
 
 // ========== 函数原型 ===========
 void Stat(char* s, int* w, int& num);
